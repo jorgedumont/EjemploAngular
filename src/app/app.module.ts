4b1178ee3c623697618458850eab9370e868cca8
Jorge Dumont <jdumontl@hotmail.com>
2021-02-15 14:18:01 +0100

    codigo

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EjemploComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
